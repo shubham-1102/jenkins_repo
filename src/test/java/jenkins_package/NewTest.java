b46750b43184c21b343502342f5c0f4dbdee5df0
shubham-1102 <shubhampatil1102@gmail.com>
2023-09-05 14:55:39 +0530

    first commit

---
+++ src/test/java/jenkins_package/NewTest.java
@@ -0,0 +1,13 @@
+package jenkins_package;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	WebDriver driver;
+	@Test
+  public void f() {
+  driver =new ChromeDriver();
+  }
+}
